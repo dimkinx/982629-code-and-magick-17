353f10c60cff253e5592311836cea839885d8741
Дмитрий <htmlacademy.ostroukhov@gmail.com>
2019-07-17 21:28:43 +0300

    Создает и подключает модуль backend.js

---
+++ js/backend.js
@@ -0,0 +1,14 @@
+'use strict';
+
+(function () {
+  var load = function (onLoad, onError) {
+  };
+
+  var save = function (data, onLoad, onError) {
+  };
+
+  window.backend = {
+    load: load,
+    save: save,
+  };
+})();
